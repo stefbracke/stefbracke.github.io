67bd5ca953888a30178372b648812de55e9ed98d
Stef <stef.br@pixel-nexus.com>
2024-10-21 23:23:59 +0200

    sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
-
-    $('h1').click(function () {
-        $(this).css('background-color', '#ff0000');
-    })
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '100px;'
+    });
 });
