361089897c40219a0d80f803e336267881087d52
Stef <stef.br@pixel-nexus.com>
2024-10-22 20:28:41 +0200

    animation support

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,6 @@
+import 'animate.css';
 $(document).ready(function () {
+    /* For the sticky navigation */
     $('.js--section-features').waypoint(function (direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -6,6 +8,37 @@ $(document).ready(function () {
             $('nav').removeClass('sticky');
         }
     }, {
-        offset: '100px;'
+        offset: '100px'
     });
+
+    /* Scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({scrollTop:
+            $('.js--section-plans').offset().top}, 1000);
+    });
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop:
+            $('.js--section-features').offset().top}, 1000);
+    });
+    
+    /* Navigation scroll */
+    $(function () {
+        $('a[href*="#"]:not([href="#"])').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({scrollTop: target.offset().top}, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* Animations on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animate__fadeIn');
+    })
+
 });
